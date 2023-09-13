7ee77ecdbddc393c709b40a0801d31c40c7cdc3a
makiwebdeveloper <makiwebdeveloper@gmail.com>
2023-09-13 20:41:47 +0200

    setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Home</h1>
+      <Button>Click</Button>
+    </main>
+  );
+}
